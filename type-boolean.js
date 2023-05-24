3d7f5ec5272de76d1471f2f68063228ab108da95
Alexzinfs <alexandrofredegoto05@gmail.com>
2023-05-24 15:30:26 -0300

    exemplos da aula 1

---
+++ type-boolean.js
@@ -0,0 +1,14 @@
+// boolean
+// true -> verdadeiro
+// false -> falso
+
+const primeiroNumero = 5;
+const SegundoNumero = 5;
+let cadastroAtivado = true;
+
+//console.log(primeiroNumero === SegundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "alura   ";
+
+console.log(texto1 === texto2);
